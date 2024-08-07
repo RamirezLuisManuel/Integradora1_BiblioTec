3d46d5036dbd6178760a08b332cb68352673e089
SekiroK5 <arin01246@gmail.com>
2024-08-07 14:39:35 -0600

    Modificamos las rutas

---
+++ server/src/routes/prestamoRoutes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import prestamoController from '../controllers/prestamoController';
+
+
+class PrestamoRoutes{
+    public router : Router = Router();//Se está creando la propiedad que guardara el objeto devuelto y se inicializa esta propiedad en la misma línea de código. 
+    
+    constructor(){
+        this.config();
+    }
+    config():void{
+        this.router.get('/',prestamoController.list);//Creando una ruta de mi aplicación del servidor para  la ruta inicial y se devuelve el mensaje Hello.
+        this.router.post('/',prestamoController.create);
+        this.router.delete('/:Id',prestamoController.delete); //aquí se indica que recibe como parámetro el id  del juego para poder eliminarlo.
+        this.router.put('/:Id',prestamoController.update);
+        this.router.get('/:Id',prestamoController.getOne);
+    }
+}
+
+const prestamoRoutes = new PrestamoRoutes()
+export default prestamoRoutes.router;
