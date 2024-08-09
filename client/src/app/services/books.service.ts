e754343de10f5d8cbf5940ad86d5002e02398178
SekiroK5 <arin01246@gmail.com>
2024-08-08 23:25:23 -0600

    Creación para la conexión de back con front, creo saber cual fue el problema

--- client/src/app/services/books.service.ts
+++ client/src/app/services/books.service.ts
@@ -7,7 +7,7 @@ import { Book } from '../models/Book';
 })
 export class BooksService {
 
-  API_URI = `http://localhost:3000/api`;   //Back-end
+  API_URI = `http://localhost:3000/api/libros`;   //Back-end
   //API_URI = `http://localhost:3000/books`;    //Front-end con acceso a datos json.
 
   constructor(private http : HttpClient){}
