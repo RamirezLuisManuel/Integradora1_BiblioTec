57430c466aecfe4c991d409456ec1e94f8a3f20a
SekiroK5 <arin01246@gmail.com>
2024-08-09 09:12:51 -0600

    Se actualizó el usario.ts junto a sus services

---
+++ client/src/app/models/Usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario{
+    Matricula:number;
+    Nombre:string;
+    ApPaterno :string;
+    ApMaterno :string;
+    Direccion :string;
+    Correo :string;
+    Telefono :string;
+    IdTipo : number;
+    Contraseña : string;
+    StaUsuario : string;
+}
